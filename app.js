275e054314fdf5097b59b8cee3d34a7b7c51b1d2
Luis Carlos <luicarlosferreira@outlook.com>
2023-03-22 19:31:42 +0000

    changes

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 require("./src/routes/index")(app);
 
 app.listen(PORT, () => {
-  console.log("Example app listening on port 3000!");
+  console.log("Example app listening on port " + PORT);
 });
