880524fccce075f6b766500d232ddc3d4d1fada7
Luis Carlos <luicarlosferreira@outlook.com>
2023-03-23 17:29:27 +0000

    changes

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -5,8 +5,13 @@ const {
 } = require("../controllers/product.controller");
 const cors = require("cors");
 
+const corsOptions = {
+  origin: `${process.env.BASE_URL}/products`,
+  optionsSucessStatus: 200,
+};
+
 exports.productsRoute = (app) => {
-  app.post("/products", cors(), create);
+  app.post("/products", cors(corsOptions), create);
 
   app.get("/products/all", get);
 
