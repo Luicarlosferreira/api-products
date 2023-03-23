9e63b6ed443e78d9c4fab5cad9ba8bb46b847899
Luis Carlos <luicarlosferreira@outlook.com>
2023-03-23 17:02:36 +0000

    changes

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.get("/products", function (req, res, next) {
+  res.json({ msg: "This is CORS-enabled for all origins!" });
+});
 const PORT = process.env.PORT || 8000;
 
 require("./src/routes/index")(app);
