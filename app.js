578a6f3178bcc6e938c81f796bca27cb65ca3247
Luis Carlos <luicarlosferreira@outlook.com>
2023-03-23 18:01:45 +0000

    changes

--- app.js
+++ app.js
@@ -7,6 +7,20 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    `${process.env.BASE_URL}/products`
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+});
 
 const PORT = process.env.PORT;
 
