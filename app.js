36cc5c9dfa6da8647236313821d969605f556433
Luis Carlos <luicarlosferreira@outlook.com>
2023-03-23 17:04:52 +0000

    changes

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const dotenv = require("dotenv");
 dotenv.config();
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 
-app.get("/products", function (req, res, next) {
+app.get("/products", cors(), function (req, res, next) {
   res.json({ msg: "This is CORS-enabled for all origins!" });
 });
 const PORT = process.env.PORT || 8000;
