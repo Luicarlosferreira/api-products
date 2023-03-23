cb57d664040fa2b42987cefbc25ce9a524c2a611
Luis Carlos <luicarlosferreira@outlook.com>
2023-03-23 17:18:02 +0000

    changes

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 const app = express();
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 
 app.get("/products", cors(), function (req, res, next) {
