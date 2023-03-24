4c903f196cf42ee1f50414b94666a2ef6805157e
Luis Carlos <luicarlosferreira@outlook.com>
2023-03-24 11:36:04 +0000

    changes

--- app.js
+++ app.js
@@ -10,6 +10,26 @@ app.use(express.json());
 
 const PORT = process.env.PORT;
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  const allowedOrigins = [
+    "http://localhost:3000",
+    "http://gamebrag.onrender.com",
+    "https://gamebrag.onrender.com",
+  ];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+  next();
+});
+
 require("./src/routes/index")(app);
 
 app.listen(PORT, () => {
--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -5,14 +5,7 @@ const {
 } = require("../controllers/product.controller");
 
 exports.productsRoute = (app) => {
-  app.post("/products", (req, res) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH,DELETE");
-    res.header("Access-Control-Allow-Credentials", "true");
-
-    create();
-  });
+  app.post("/products", create());
 
   app.get("/products/all", get);
 
