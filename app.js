07637ecd28077874fe2ad714acddce92c822a5c9
Luis Carlos <luicarlosferreira@outlook.com>
2023-03-24 15:33:01 +0000

    changes

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 
 const PORT = process.env.PORT;
