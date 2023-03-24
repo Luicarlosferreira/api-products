8b243505703bbe7ec2653b7c6ea319bc965f242d
Luis Carlos <luicarlosferreira@outlook.com>
2023-03-24 15:04:24 +0000

    changes

--- app.js
+++ app.js
@@ -6,25 +6,16 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-const BASE = process.env.BASE_URL;
+
 const PORT = process.env.PORT;
 
-// const options = {
-//   credentials: true,
-//   origin: ["*"],
-//   methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
-// };
+const options = {
+  credentials: true,
+  origin: ["*", "http://localhost:5173"],
+  methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
+};
 
-// app.use(cors(options));
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+app.use(cors(options));
 
 require("./src/routes/index")(app);
 
