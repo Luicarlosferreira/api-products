cb69136044d0b49e6c42e690ea0f1584d6be0872
Luis Carlos <luicarlosferreira@outlook.com>
2023-03-24 11:20:32 +0000

    changes

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -10,9 +10,7 @@ exports.productsRoute = (app) => {
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH,DELETE");
     res.header("Access-Control-Allow-Credentials", "true");
-    if (req.method === "OPTIONS") {
-      return res.send(204);
-    }
+
     create();
   });
 
