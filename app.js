dd031edf2941e750deec2016acf56afb98c63fb0
Luis Carlos <luicarlosferreira@outlook.com>
2023-03-24 10:56:28 +0000

    changes

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH,DELETE");
+  res.header("Access-Control-Allow-Credentials", "true");
   if (req.method === "OPTIONS") {
     return res.send(204);
   }
