cb56acb64ebc643815aecd7e513c66ae8aa3bb75
Luis Carlos <luicarlosferreira@outlook.com>
2023-03-24 11:13:25 +0000

    changes

--- app.js
+++ app.js
@@ -8,17 +8,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH,DELETE");
-  res.header("Access-Control-Allow-Credentials", "true");
-  if (req.method === "OPTIONS") {
-    return res.send(204);
-  }
-  next();
-});
-
 const PORT = process.env.PORT;
 
 require("./src/routes/index")(app);
--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -5,7 +5,16 @@ const {
 } = require("../controllers/product.controller");
 
 exports.productsRoute = (app) => {
-  app.post("/products", create);
+  app.post("/products", (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH,DELETE");
+    res.header("Access-Control-Allow-Credentials", "true");
+    if (req.method === "OPTIONS") {
+      return res.send(204);
+    }
+    create();
+  });
 
   app.get("/products/all", get);
 
