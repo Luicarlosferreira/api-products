63b3850b5d9ca345087e3c405c2a03835f913f7c
Luis Carlos <luicarlosferreira@outlook.com>
2023-03-27 12:32:36 +0100

    changes

--- app.js
+++ app.js
@@ -3,10 +3,23 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 
 dotenv.config();
+app.use(cors());
 
 const app = express();
-app.use(cors());
+
 app.use(express.json());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
 
 const PORT = process.env.PORT;
 
