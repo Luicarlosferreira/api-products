d9f69783189db72e780bbc0062965286e86be311
Luis Carlos <luicarlosferreira@outlook.com>
2023-03-29 13:16:20 +0100

    changes

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -6,12 +6,17 @@ const {
 
 exports.productsRoute = (app) => {
   app.post("/products", create);
-
   app.get("/products", get);
-
   app.get("/products/unique", getUniqueById);
-
   app.get("/", (req, res) => {
     res.send("hello world");
+  });
+  app.post("/teste", (req, res) => {
+    const { name, color } = req.params;
+
+    res.send({
+      name,
+      color,
+    });
   });
 };
