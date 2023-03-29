c2621f3d2cab117d75cb388ffd4bdbdc6de3bb6e
Luis Carlos <luicarlosferreira@outlook.com>
2023-03-29 10:50:03 +0100

    changes

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-
+const OPTIONS = {
+  origin: "http://localhost:5173",
+};
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(cors(OPTIONS));
 
 const PORT = process.env.PORT || 3333;
-app.use(cors());
 
 require("./src/routes/index")(app);
 
