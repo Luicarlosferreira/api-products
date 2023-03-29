6ec7f07a01da7198b9d98b4348fcfa68b616c44b
Luis Carlos <luicarlosferreira@outlook.com>
2023-03-29 11:17:48 +0100

    changes

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -5,8 +5,9 @@ const {
 } = require("../repositories/product.repository");
 
 exports.create = async (req, res) => {
+  const { name, color, size } = req.body;
   try {
-    const products = await createProduct(req.body);
+    const products = await createProduct(name, color, size);
     res.status(200).send(products);
   } catch (error) {
     res.status(404).send(error);
